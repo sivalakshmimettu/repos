436c2812279fb0757580f431a68b5799547512b1
Prayankag <131856399+Prayankag@users.noreply.github.com>
2025-01-06 15:17:24 +0530

    Update main.tf

--- webapp/modules/webapp/main.tf
+++ webapp/modules/webapp/main.tf
@@ -15,10 +15,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id ="f936a180-7b93-4203-8faa-f376529bd4f8"
-  client_id       ="a2d89136-b086-4755-9f98-af856c2d8c30"
-  client_secret  = "QEt8Q~el5WQ8JWv56IsRMvM5mSJeMHsflwyEJbQ7"
-  tenant_id      = "13085c86-4bcb-460a-a6f0-b373421c6323"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret  = var.client_secret
+  tenant_id      = var.tenant_id
 }
 
 
