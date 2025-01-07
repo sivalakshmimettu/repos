a2579d3ba84cdcc422412ed73a29e2b34ec8d1da
nareshreddivari <167742965+nareshreddivari@users.noreply.github.com>
2025-01-07 16:10:28 +0530

    Update main.tf

--- webapp/modules/webapp/main.tf
+++ webapp/modules/webapp/main.tf
@@ -17,7 +17,7 @@ provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
   client_id       = var.client_id
-  clientsecret  = var.client_secret
+  client_secret  = var.client_secret
   tenant_id      = var.tenant_id
 }
 
