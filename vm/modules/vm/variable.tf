827bae17045d5612e33b683d8235545803980d21
sivalakshmimettu <sivalakshmi.mettu@valuemomentum.com>
2025-01-08 15:32:55 +0530

    Update variable.tf

--- vm/modules/vm/variable.tf
+++ vm/modules/vm/variable.tf
@@ -40,3 +40,32 @@ variable "admin_password" {
   type        = string
   default     = "default-password"  # Set a secure default value
 }
+variable "subscription_id" {
+  type        = string
+  default     = "f936a180-7b93-4203-8faa-f376529bd4f8"  # Use "Windows" for a Windows app
+}
+variable "client_id" {
+  type        = string
+  default     = "a2d89136-b086-4755-9f98-af856c2d8c30"  # Use "Windows" for a Windows app
+}
+variable "client_secret" {
+  type        = string
+  default     = " "
+  }
+variable "tenant_id" {
+  type        = string
+  default     = "13085c86-4bcb-460a-a6f0-b373421c6323"  # Use "Windows" for a Windows app
+}
+# Define the Key Vault name (assuming Key Vault is created manually)
+variable "key_vault_name" {
+  description = "The name of the Azure Key Vault"
+  type        = string
+  default     = "GitHubKVault"  # Name of your manually created Key Vault
+}
+
+# Define the name of the secret to store in Key Vault
+variable "secret_name" {
+  description = "The name of the secret to store in Key Vault"
+  type        = string
+  default     = "clientsecret1"
+}
