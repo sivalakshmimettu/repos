b93e5be08fce16929654821cbb896b9c373061fc
sivalakshmimettu <sivalakshmi.mettu@valuemomentum.com>
2025-01-08 15:34:10 +0530

    Update main.tf

--- vm/main.tf
+++ vm/main.tf
@@ -1,22 +1,21 @@
 # terraform/main.tf
-# Define the resource group
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
+# Reference the existing resource group
+data "azurerm_resource_group" "example" {
+  name = var.resource_group_name
 }
 provider "azurerm" {
   features {}
-  subscription_id ="f936a180-7b93-4203-8faa-f376529bd4f8"
-  client_id       ="a2d89136-b086-4755-9f98-af856c2d8c30"
-  client_secret  = "QEt8Q~el5WQ8JWv56IsRMvM5mSJeMHsflwyEJbQ7"
-  tenant_id      = "13085c86-4bcb-460a-a6f0-b373421c6323"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  clientsecret  = var.client_secret
+  tenant_id      = var.tenant_id
 }
 
 module "vm" {
   source = "./modules/vm"
 
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name      = data.azurerm_resource_group.example.name
+  location                 = data.azurerm_resource_group.example.location
   vm_name                  = var.vm_name
   vm_size                  = var.vm_size
   admin_username           = var.admin_username
