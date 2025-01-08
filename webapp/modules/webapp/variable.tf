e370e02cf32e6934741d08d5a03aae8ae4e404b2
sivalakshmimettu <sivalakshmi.mettu@valuemomentum.com>
2025-01-08 16:17:35 +0530

    Update variable.tf

--- webapp/modules/webapp/variable.tf
+++ webapp/modules/webapp/variable.tf
@@ -6,11 +6,11 @@ variable "resource_group_name" {
   default     = "Geicoautomationscript"
 }
 
-#variable "location" {
-  #description = "The Azure region where resources will be created"
-  #type        = string
-  #default     = "East US"
-#}
+variable "location" {
+  description = "The Azure region where resources will be created"
+  type        = string
+  default     = "East US"
+}
 
 variable "app_service_plan_name" {
   description = "The name of the App Service Plan"
