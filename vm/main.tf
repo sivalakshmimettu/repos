3205f6e7abc708d31bd773db64e1c3ef652e77a9
sivalakshmimettu <sivalakshmi.mettu@valuemomentum.com>
2025-02-06 21:51:35 +0530

    Update main.tf

--- vm/main.tf
+++ vm/main.tf
@@ -50,7 +50,7 @@ resource "azurerm_network_security_group" "example" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = "*"
+    source_address_prefix      = "203.0.113.0/24"
     destination_address_prefix = "*"
   }
 
